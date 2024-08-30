5ede65f1b03d5e9eac97c2a0541ee3b1889f8c5c
dorilama <assioma.xi@gmail.com>
2024-08-30 13:09:57 +0100

    implement useTopicEffect

--- dist/module/index.js
+++
@@ -1,9 +0,0 @@
-// Notice:
-// adapted from [@instantdb/react](https://github.com/instantdb/instant/blob/main/client/packages/react/README.md)
-// see instantdb-license.md for license
-import { id, tx, lookup, i, } from "@instantdb/core";
-import { InstantVue } from "./InstantVue";
-import { init } from "./init";
-// import { Cursors } from "./Cursors";
-export { id, tx, lookup, init, InstantVue, i, };
-//# sourceMappingURL=index.js.map
