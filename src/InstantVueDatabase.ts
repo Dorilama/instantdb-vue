5090afca33765aa2c393e93a4b8f05d28c31de1f
dorilama <assioma.xi@gmail.com>
2025-03-14 12:04:14 +0000

    feat: add `ruleParams` to `queryOnce`

--- src/InstantVueDatabase.ts
+++ src/InstantVueDatabase.ts
@@ -285,11 +285,12 @@ export class InstantVueDatabase<
    *  console.log(resp.data.goals)
    */
   queryOnce = <Q extends InstaQLParams<Schema>>(
-    query: Q
+    query: Q,
+    opts?: InstaQLOptions
   ): Promise<{
     data: InstaQLResponse<Schema, Q>;
     pageInfo: PageInfoResponse<Q>;
   }> => {
-    return this._core.queryOnce(query);
+    return this._core.queryOnce(query, opts);
   };
 }
