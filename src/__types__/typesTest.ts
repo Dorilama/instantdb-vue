48cfb205cca0bf2a0ef782ccbecf9bc813ee0782
dorilama <assioma.xi@gmail.com>
2025-12-10 15:38:44 +0100

    add type tests from @instantdb/react

---
+++ src/__types__/typesTest.ts
@@ -0,0 +1,90 @@
+// Notice:
+// adapted from [@instantdb/react](https://github.com/instantdb/instant/blob/main/client/packages/react/README.md)
+// see instantdb-license.md for license
+
+import type { Equal, Expect, IsAny, NotAny } from "./typeUtils.ts";
+import { i, init } from "../index";
+
+const schema = i.schema({
+  entities: {
+    tbl: i.entity({
+      d: i.date().indexed(),
+      dOptional: i.date().optional(),
+    }),
+  },
+});
+
+function _testUseDatesTest() {
+  const db = init({
+    schema,
+    appId: "123",
+    useDateObjects: true,
+  });
+
+  const { data } = db.useQuery({ tbl: {} });
+  const item = data.value?.tbl[0];
+  if (item) {
+    type t = typeof item.d;
+    type _cases = [Expect<NotAny<t>>, Expect<Equal<t, Date>>];
+
+    type tOpt = typeof item.dOptional;
+    type _cases_2 = [
+      Expect<NotAny<tOpt>>,
+      Expect<Equal<tOpt, Date | undefined>>
+    ];
+  }
+}
+
+function _testUseDatesFalseTest() {
+  const db = init({
+    schema,
+    appId: "123",
+    useDateObjects: false,
+  });
+
+  const { data } = db.useQuery({ tbl: {} });
+  const item = data.value?.tbl[0];
+  if (item) {
+    type t = typeof item.d;
+    type _cases = [Expect<NotAny<t>>, Expect<Equal<t, string | number>>];
+
+    type tOpt = typeof item.dOptional;
+    type _cases_2 = [
+      Expect<NotAny<tOpt>>,
+      Expect<Equal<tOpt, string | number | undefined>>
+    ];
+  }
+}
+
+function _testUseDatesUndefinedTest() {
+  const db = init({
+    schema,
+    appId: "123",
+  });
+
+  const { data } = db.useQuery({ tbl: {} });
+  const item = data.value?.tbl[0];
+  if (item) {
+    type t = typeof item.d;
+    type _cases = [Expect<NotAny<t>>, Expect<Equal<t, string | number>>];
+
+    type tOpt = typeof item.dOptional;
+    type _cases_2 = [
+      Expect<NotAny<tOpt>>,
+      Expect<Equal<tOpt, string | number | undefined>>
+    ];
+  }
+}
+
+function _testDataNoSchema() {
+  const db = init({
+    appId: "123",
+  });
+
+  const { data } = db.useQuery({ tbl: {} });
+  const item = data.value?.tbl[0];
+  if (item) {
+    type t = typeof item.d;
+    type _cases = [Expect<IsAny<t>>, Expect<Equal<typeof item.id, string>>];
+  }
+}
